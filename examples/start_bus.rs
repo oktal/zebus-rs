c891696eb0a1524fa449abe3ffb52d98547f3374
Mathieu Stefani <auquetal@gmail.com>
2024-01-15 17:50:24 +0100

    chore(examples): use `tracing` in examples

--- examples/start_bus.rs
+++ examples/start_bus.rs
@@ -5,8 +5,18 @@ use tokio;
 use zebus::transport::zmq::{ZmqSocketOptions, ZmqTransport, ZmqTransportConfiguration};
 use zebus::{Bus, BusBuilder};
 
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
+
+use tracing::error;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_env("ZEBUS_LOG"))
+        .init();
+
     let zmq_configuration = ZmqTransportConfiguration {
         inbound_endpoint: "tcp://*:*".into(),
         wait_for_end_of_stream_ack_timeout: Duration::from_secs(10),
@@ -21,7 +31,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .unwrap();
 
     if let Err(e) = bus.start().await {
-        eprintln!("{e}");
+        error!("{e}");
     }
 
     println!("Press a key to exit");
