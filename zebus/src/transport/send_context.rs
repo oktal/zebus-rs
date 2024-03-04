b1a33e84f0f4beb95a029e53fcbaaebe9431fbde
Mathieu Stefani <auquetal@gmail.com>
2024-03-04 09:52:59 +0100

    chore(transport): properly document SendContext

--- zebus/src/transport/send_context.rs
+++ zebus/src/transport/send_context.rs
@@ -1,14 +1,21 @@
 use crate::{Peer, PeerId};
 
+/// Represents the context associated with message sending
+/// This struct is used to encapsulate useful information
+/// for the [`crate::Transport`] layer when sending a message
+/// to a list of recipient peers
 #[derive(Debug)]
 pub struct SendContext {
-    pub persistent_peer_ids: Vec<PeerId>,
+    /// List of target peers that are persistent
+    pub(crate) persistent_peer_ids: Vec<PeerId>,
 
-    pub persistent_peer: Option<Peer>,
+    /// [`Peer`] of the persistence service
+    /// When [`None`], the persistence service peer is unknown
+    pub(crate) persistence_peer: Option<Peer>,
 }
 
 impl SendContext {
-    pub fn was_persisted(&self, peer_id: &PeerId) -> bool {
+    pub(crate) fn was_persisted(&self, peer_id: &PeerId) -> bool {
         self.persistent_peer_ids
             .iter()
             .find(|&p| p == peer_id)
@@ -20,7 +27,7 @@ impl Default for SendContext {
     fn default() -> Self {
         Self {
             persistent_peer_ids: vec![],
-            persistent_peer: None,
+            persistence_peer: None,
         }
     }
 }
